a7c251bf01070d4b58e1dbf8c0a758d1d372a511
devwithzain <zainsoftwear11@gmail.com>
2024-08-05 19:33:22 +0500

    modified

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 export { default as Hero } from './hero';
 export { default as Menu } from './menu';
 export { default as Footer } from './footer';
+export { default as Information } from './inform';
+export { default as Portfolio } from './portfolio';
 export { default as HorizontalSlider } from './horizontal-slider';
