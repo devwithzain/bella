f1b0405f5bf653b9868141c49281a7ebc37e4ed1
devwithzain <ZainSoftwear11@gmail.com>
2024-08-09 06:42:31 +0500

    modified

--- animations/index.ts
+++ animations/index.ts
@@ -1,2 +1,3 @@
+export { default as PreLoad } from './PreLoad';
 export { default as TextMask } from './TextMask';
 export { default as TextMarquee } from './text-marquee';
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,17 @@
+import { StaticImageData } from "next/image";
+
 export type TlogoMarqueeProps = {
    children: React.ReactNode;
    baseVelocity: number;
+};
+
+export type TmodalProps = {
+   modal: {
+      active: boolean;
+      index: number;
+   };
+   projects: {
+      img: StaticImageData;
+      color: string;
+   }[];
 };
