d15d19de575a163c8d2f6296a260cfcbbcc82c8e
devwithzain <ZainSoftwear11@gmail.com>
2024-08-10 00:37:43 +0500

    modified

--- components/index.ts
+++ components/index.ts
@@ -5,3 +5,6 @@ export { default as Information } from './inform';
 export { default as DesignsSlider } from './designs-slider';
 export { default as Portfolio } from './portfolio';
 export { default as HorizontalSlider } from './horizontal-slider';
+export { default as AboutPage } from './about';
+export { default as PortfolioPage } from './portfolio-page';
+export { default as ContactPage } from './contact-page';
